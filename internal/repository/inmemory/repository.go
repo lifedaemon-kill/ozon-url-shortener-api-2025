dae85f2ecb517444f033916da9afe61b22330228
Ruslan <noname0263@gmail.com>
2025-10-05 11:37:34 +0300

    fix

--- internal/repository/inmemory/repository.go
+++ internal/repository/inmemory/repository.go
@@ -4,35 +4,45 @@ import (
 	"context"
 	"main/internal/pkg/errs"
 	"main/internal/repository/entities"
+	"sync"
 )
 
 type UrlRepository struct {
-	dict map[string]string
+	shortToOrigin sync.Map
+	originToShort sync.Map
+	mu            sync.Mutex
 }
 
 func New() *UrlRepository {
-	return &UrlRepository{dict: make(map[string]string)}
+	return &UrlRepository{}
 }
 
 func (r *UrlRepository) AddUrlRelation(ctx context.Context, req entities.AddUrlRelationRequest) (entities.AddUrlRelationResponse, error) {
-	if _, exist := r.dict[req.ShortenedUrl]; exist {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if _, exist := r.shortToOrigin.Load(req.ShortenedUrl); exist {
 		return entities.AddUrlRelationResponse{}, errs.ErrorRepositoryDuplicate
 	}
-	for _, v := range r.dict {
-		if v == req.OriginUrl {
-			return entities.AddUrlRelationResponse{}, errs.ErrorAlreadyExist
-		}
+
+	if _, exist := r.originToShort.Load(req.OriginUrl); exist {
+		return entities.AddUrlRelationResponse{}, errs.ErrorAlreadyExist
 	}
-	r.dict[req.ShortenedUrl] = req.OriginUrl
+
+	r.shortToOrigin.Store(req.ShortenedUrl, req.OriginUrl)
+	r.originToShort.Store(req.OriginUrl, req.ShortenedUrl)
+
 	return entities.AddUrlRelationResponse{ShortenedUrl: req.ShortenedUrl}, nil
 }
 
 func (r *UrlRepository) GetOriginURLFromShortened(ctx context.Context, req entities.GetOriginURLFromShortenedUrlRequest) (entities.GetOriginURLFromShortenedUrlResponse, error) {
-	origin, exist := r.dict[req.ShortenedUrl]
+	origin, exist := r.shortToOrigin.Load(req.ShortenedUrl)
 	if !exist {
-		return entities.GetOriginURLFromShortenedUrlResponse{""}, errs.ErrorRepositoryUrlEmpty
+		return entities.GetOriginURLFromShortenedUrlResponse{OriginUrl: ""}, errs.ErrorRepositoryUrlEmpty
 	}
-	return entities.GetOriginURLFromShortenedUrlResponse{origin}, nil
+
+	val, _ := origin.(string)
+	return entities.GetOriginURLFromShortenedUrlResponse{OriginUrl: val}, nil
 }
 
 func (r *UrlRepository) Close() error {
